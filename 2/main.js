55496c2c94ab2c64b26b1dfc20829e72838c5da7
brokenfingers <buvotrys@gmail.com>
2022-05-30 01:53:39 +0300

    2 uzduotis

---
+++ 2/main.js
@@ -0,0 +1,78 @@
+//1 ----------------------------------------------------------------------
+const resultHolder = document.createElement('h2')
+const body = document.querySelector('body')
+body.appendChild(resultHolder)
+const button1 = document.createElement('button')
+button1.textContent = 'Rodyti žinutę'
+button1.addEventListener('click', showMessage)
+body.appendChild(button1)
+
+
+function showMessage() {
+    resultHolder.textContent = 'Mėgstu programuoti.Yey'
+}
+
+
+
+
+
+//2 ----------------------------------------------------------------------
+
+const button2 = document.createElement('button')
+button2.textContent = 'Apskaiciuoti'
+body.appendChild(button2)
+button2.addEventListener('click', () => countPrice('obuoliai', 0.7))
+
+function countPrice(title, price) {
+    resultHolder.textContent = `5 x ${title} = ${price * 5}`
+}
+
+//3 ----------------------------------------------------------------------
+
+const button3 = document.createElement('button')
+button3.textContent = 'Apskaiciuoti2'
+body.appendChild(button3)
+button3.addEventListener('click', countPrice2)
+
+
+const input1 = document.createElement('input')
+input1.type = 'text';
+input1.placeholder = 'Vaisiaus pavadinimas'
+const input2 = document.createElement('input')
+input2.type = 'text';
+input2.placeholder = 'Kaina'
+const input3 = document.createElement('input')
+input3.type = 'text';
+input3.placeholder = 'kiekis'
+body.appendChild(input1)
+body.appendChild(input2)
+body.appendChild(input3)
+
+function countPrice2() {
+    if (input1.value == '' || input2.value == '' || input3.value == '') {
+        alert("uzpildyti langelius")
+        return
+    }
+    resultHolder.textContent = `${+input3.value} x ${input1.value}(${+input2.value}) = ${+input2.value * +input3.value}`
+}
+
+
+//4 ----------------------------------------------------------------------
+const heroeHolder = document.createElement('div')
+body.appendChild(heroeHolder)
+
+const heroesArr = [{ name: 'pelene', properties: 'dazniausiai be vieno bato' },
+{ name: 'Miegancioji gražuolė', properties: 'pirmadieniais į mokyklą neatvyksta, nes pramiega' },
+{ name: 'Septyni nykštukai', properties: 'stiprūs kol vaikšto septyniese' },
+{ name: 'Ragana', properties: 'nera linkusi į gerus darbus' },
+{ name: 'Pinokis', properties: 'pasakų pirikūpas' },
+{ name: 'Karlsonas', properties: 'pastoviai išskridęs' }]
+
+let html = '<ul>';
+
+heroesArr.forEach((itm, i) => {
+    html += `<li style="background-color:${i % 2 == 0 ? 'blue' : 'none'}; color: ${i % 2 == 0 ? 'white' : 'black'}">${itm.name}  -  ${itm.properties}</li>`
+})
+html += '</ul>'
+
+heroeHolder.innerHTML = html;
